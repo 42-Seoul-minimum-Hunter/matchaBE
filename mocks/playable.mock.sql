6b7ffb272bdd4c858de47cebdd16d1c3832c5ce0
yemin4242 <whereismysejong>
2024-08-18 01:17:29 +0900

    refactor: user rate logic

--- mocks/playable.mock.sql
+++ mocks/playable.mock.sql
@@ -106,27 +106,27 @@ VALUES
 INSERT INTO user_ratings
 (user_id, rated_id, rate_score, rated_at)
 VALUES
-(701, 699, 3, '2020-01-01 00:00:00');
-
-INSERT INTO user_ratings
-(user_id, rated_id, rate_score, rated_at)
-VALUES
-(702, 699, 4, '2020-01-01 00:00:00');
-
-INSERT INTO user_ratings
-(user_id, rated_id, rate_score, rated_at)
-VALUES
-(701, 696, 3, '2020-01-01 00:00:00');
-
-INSERT INTO user_ratings
-(user_id, rated_id, rate_score, rated_at)
-VALUES
-(702, 696, 4, '2020-01-01 00:00:00');
-
-INSERT INTO user_ratings
-(user_id, rated_id, rate_score, rated_at)
-VALUES
 (699, 701, 3, '2020-01-01 00:00:00');
+
+INSERT INTO user_ratings
+(user_id, rated_id, rate_score, rated_at)
+VALUES
+(699, 702,  4, '2020-01-01 00:00:00');
+
+INSERT INTO user_ratings
+(user_id, rated_id, rate_score, rated_at)
+VALUES
+(696, 701, 3, '2020-01-01 00:00:00');
+
+INSERT INTO user_ratings
+(user_id, rated_id, rate_score, rated_at)
+VALUES
+(696, 702, 4, '2020-01-01 00:00:00');
+
+INSERT INTO user_ratings
+(user_id, rated_id, rate_score, rated_at)
+VALUES
+(701, 699, 3, '2020-01-01 00:00:00');
 
 
 INSERT INTO user_like_histories
