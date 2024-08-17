baef4150349625c6b387c7bcb1fb04d242164c8d
yemin4242 <whereismysejong>
2024-08-18 01:20:24 +0900

    feat: user cannot report with liked

--- services/user.report.service.js
+++ services/user.report.service.js
@@ -1,6 +1,7 @@
 const userReportRepository = require("../repositories/user.report.repository");
 const userRepository = require("../repositories/user.repository");
 const userBlockRepository = require("../repositories/user.block.repository");
+const userLikeRepository = require("../repositories/user.like.repository");
 
 const logger = require("../configs/logger");
 
@@ -17,6 +18,17 @@ const reportUser = async (reportedUsername, userId) => {
       error.status = 404;
       throw error;
     }
+
+    const likeInfo = await userLikeRepository.getLikeUserHistoryById(
+      userId,
+      blockedUserInfo.id
+    );
+    if (likeInfo.length > 0) {
+      const error = new Error("You cannot block a user you like.");
+      error.status = 400;
+      throw error;
+    }
+
     await userReportRepository.reportUser(reportedUserInfo.id, userId);
     await userBlockRepository.addBlockUser(reportedUserInfo.id, userId);
   } catch (error) {
